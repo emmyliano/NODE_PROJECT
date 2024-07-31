4a7cc00f8389b25d7843fbd7ae0986138276b72f
Emmyliano <akemuemmy@gmail.com>
2024-07-31 20:24:39 +0100

    First commit

---
+++ config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+
+const connection = mongoose.createConnection('mongodb://localhost:27017').on('open',()=>{
+    console.log("MongoDb Connected");
+}).on('error',()=>{
+    console.log("MongoDb connection error");
+})
+
+
+module.exports = connection;

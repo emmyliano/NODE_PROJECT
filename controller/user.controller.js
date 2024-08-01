35dceaf26b7e9a5e8c0a89d7f7d7b11e231798ef
Emmyliano <akemuemmy@gmail.com>
2024-08-01 10:30:24 +0100

    added login code

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -5,10 +5,36 @@ exports.register = async(req,res,next)=>{
     try {
         const {email,password} = req.body;
 
-        // console.log(UserService);
         const successRes = await UserService.registerUser(email,password);
 
         res.json({status:true,success:"User Registered Successfully"});
+    } catch (error) {
+        throw error
+    }
+}
+
+exports.login = async(req,res,next)=>{
+    try {
+        const {email,password} = req.body;
+
+        const user = await UserService.checkuser(email);
+
+        if(!user){
+            throw new Error('User does not exist');
+        }
+
+        const isMatch = await user.comparePassword(password);
+
+        if(isMatch === false){
+            throw new Error('Password Invalid');
+        }
+
+        let tokenData = {_id:user._id,email:user.email};
+
+        const token = await UserService.generateToken(tokenData,"secretKey",'1h')
+
+        res.status(200).json({status:true,token:token})
+        
     } catch (error) {
         throw error
     }
--- services/user.services.js
+++ services/user.services.js
@@ -1,4 +1,5 @@
 const UserModel = require('../model/user.model')
+const jwt = require('jsonwebtoken');
 
 class UserService{
     static async registerUser(email,password){
@@ -9,6 +10,19 @@ class UserService{
             throw err;
         }
     }
+
+    static async checkuser(email){
+        try {
+            return await UserModel.findOne({email});
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    static async generateToken(tokenData,secretKey,jwt_expire){
+        return jwt.sign(tokenData,secretKey,{expiresIn:jwt_expire});
+    }
+
 }
 
 module.exports = UserService;
